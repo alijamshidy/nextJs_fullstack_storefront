40aefb75c93934914783a893c253d01a232ac892
alijamshidy <kbxxkj@gmail.com>
2025-11-19 05:43:22 +0330

    SectionTitle and EmptyList Components

--- components/global/EmptyList.tsx
+++ components/global/EmptyList.tsx
@@ -1,3 +1,11 @@
-export default function EmptyList() {
-  return <div>EmptyList</div>;
+import { cn } from "@/lib/utils";
+
+export default function EmptyList({
+  heading = "No items found.",
+  className,
+}: {
+  heading?: string;
+  className?: string;
+}) {
+  return <h2 className={cn("text-xl", className)}>{heading}</h2>;
 }
