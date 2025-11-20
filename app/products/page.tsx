d12a58d7d2e225372e807b651ec6c42f8d4f2a29
alijamshidy <kbxxkj@gmail.com>
2025-11-20 23:26:21 +0330

    ProductsContainer Component

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,3 +1,26 @@
-export default function ProductsPage() {
-  return <div>ProductsPage</div>;
+import ProductsContainer from "@/components/products/ProductsContainer";
+import { use } from "react";
+
+export default function Page({
+  searchParams,
+}: {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}) {
+  const filters = use(searchParams);
+
+  const search = Array.isArray(filters.search)
+    ? filters.search[0] ?? ""
+    : filters.search ?? "";
+  const layout = Array.isArray(filters.layout)
+    ? filters.layout[0] ?? "grid"
+    : filters.layout ?? "grid";
+
+  return (
+    <>
+      <ProductsContainer
+        layout={layout}
+        search={search}
+      />
+    </>
+  );
 }
