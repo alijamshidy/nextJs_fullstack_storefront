921f4356928180bd2a37c0ba1a19b0de5a89b3f6
alijamshidy <kbxxkj@gmail.com>
2025-11-21 01:43:40 +0330

    Implement search functionality in NavSearch component with debouncing

--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,11 +1,39 @@
+"use client";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { useDebouncedCallback } from "use-debounce";
 import { Input } from "../ui/input";
 
 export default function NavSearch() {
+  const searchParams = useSearchParams();
+  const { replace } = useRouter();
+  const [search, setSearch] = useState(
+    searchParams.get("search")?.toString() || ""
+  );
+  const handleSearch = useDebouncedCallback(value => {
+    const params = new URLSearchParams(searchParams);
+    if (value) {
+      params.set("search", value);
+    } else {
+      params.delete("search");
+    }
+    replace(`/products?${params.toString()}`);
+  }, 500);
+  useEffect(() => {
+    if (!searchParams.get("search")) {
+      setTimeout(() => setSearch(""), 0);
+    }
+  }, [searchParams]);
   return (
     <Input
       type="search"
       placeholder="search product..."
       className="max-w-xs dark:bg-muted"
+      value={search}
+      onChange={e => {
+        setSearch(e.target.value);
+        handleSearch(e.target.value);
+      }}
     />
   );
 }
