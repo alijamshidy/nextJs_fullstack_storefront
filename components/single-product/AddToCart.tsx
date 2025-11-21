8646b919dbd13ce45e14be67e3b5144a586a107e
alijamshidy <kbxxkj@gmail.com>
2025-11-21 17:27:32 +0330

    Add AddToCart component to handle product addition to cart

---
+++ components/single-product/AddToCart.tsx
@@ -0,0 +1,5 @@
+import { Button } from "../ui/button";
+
+export default function AddToCart({ productId }: { productId: string }) {
+  return <Button className="capitalize mt-8">Add to cart</Button>;
+}
