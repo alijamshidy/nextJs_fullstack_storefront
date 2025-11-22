e37b5e1719fcf5e01bb01dbb3552e648f7704e27
alijamshidy <kbxxkj@gmail.com>
2025-11-22 16:30:08 +0330

    Refactor database connection setup to use PrismaPg adapter and improve singleton handling

--- utils/db.ts
+++ utils/db.ts
@@ -1,17 +1,23 @@
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
+import { Pool } from "pg";
 
-const prismaClientSingleton = () => {
-  return new PrismaClient();
-};
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL!,
+});
 
-type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+const adapter = new PrismaPg(pool);
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClientSingleton | undefined;
+  prisma: PrismaClient | undefined;
 };
 
-const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
-
-export default prisma;
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+  });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export default prisma;
