e360d6c1b7262e9c717292999c3866ee2d5b5971
alijamshidy <kbxxkj@gmail.com>
2025-11-24 01:45:42 +0330

    Add Sidebar component for admin navigation

---
+++ app/admin/Sidebar.tsx
@@ -0,0 +1,27 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { adminLinks } from "@/utils/links";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function Sidebar() {
+  const pathName = usePathname();
+
+  return (
+    <aside>
+      {adminLinks.map(link => {
+        const isActivePage = pathName === link.href;
+        const variant = isActivePage ? "default" : "ghost";
+        return (
+          <Button
+            key={link.href}
+            asChild
+            className="w-full mb-2 capitalize font-normal"
+            variant={variant}>
+            <Link href={link.href}>{link.label}</Link>
+          </Button>
+        );
+      })}
+    </aside>
+  );
+}
