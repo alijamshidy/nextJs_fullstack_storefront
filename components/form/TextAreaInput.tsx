d4200e12c0cd67ce32d27543d56645c8faee85bb
alijamshidy <kbxxkj@gmail.com>
2025-12-04 21:59:17 +0330

    Add TextAreaInput component for reusable text area input with label support

---
+++ components/form/TextAreaInput.tsx
@@ -0,0 +1,27 @@
+import { Label } from "../ui/label";
+import { Textarea } from "../ui/textarea";
+
+type TextAreaInputProps = {
+  name: string;
+  labelText?: string;
+  defaultValue?: string;
+};
+function TextAreaInput({ name, labelText, defaultValue }: TextAreaInputProps) {
+  return (
+    <div className="mb-2">
+      <Label
+        className="capitalize mb-2"
+        htmlFor={name}>
+        {labelText || name}
+      </Label>
+      <Textarea
+        className="resize-none leading-loose"
+        rows={5}
+        id={name}
+        name={name}
+        defaultValue={defaultValue}
+        required
+      />
+    </div>
+  );
+}
