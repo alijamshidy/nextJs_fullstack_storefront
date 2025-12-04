cbc0c299e8cbba40e19c37ff8c485bbdaa958cb3
alijamshidy <kbxxkj@gmail.com>
2025-12-04 21:58:53 +0330

    Add CheckboxInput component for reusable checkbox input with label support

---
+++ components/form/CheckboxInput.tsx
@@ -0,0 +1,24 @@
+import { Checkbox } from "../ui/checkbox";
+import { Label } from "../ui/label";
+
+type CheckboxInputProps = {
+  name: string;
+  label: string;
+  defaultChecked?: boolean;
+};
+function CheckboxInput({ name, label, defaultChecked }: CheckboxInputProps) {
+  return (
+    <div className="mb-2 flex items-center space-x-2">
+      <Checkbox
+        id={name}
+        name={name}
+        defaultChecked={defaultChecked}
+      />
+      <Label
+        className="capitalize text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 "
+        htmlFor={name}>
+        {label}
+      </Label>
+    </div>
+  );
+}
