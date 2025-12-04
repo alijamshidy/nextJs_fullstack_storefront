226d389a56444b96c1e5a4190cb1e608915c0ae4
alijamshidy <kbxxkj@gmail.com>
2025-12-04 23:43:01 +0330

    Add authentication and image upload functionality to createProductAction, enhancing product creation process with user validation and Cloudinary integration.

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,7 +1,27 @@
 "use server";
+import { currentUser } from "@clerk/nextjs/server";
+import { v2 as cloudinary } from "cloudinary";
 import { redirect } from "next/navigation";
 import prisma from "./db";
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
+});
 
+const getAuthUser = async () => {
+  const user = await currentUser();
+  if (!user) {
+    redirect("/");
+  }
+  return user;
+};
+const renderError = (error: unknown): { message: string } => {
+  console.log(error);
+  return {
+    message: error instanceof Error ? error.message : "there was an error...",
+  };
+};
 export const fetchFeaturedProducts = async () => {
   const products = await prisma?.product?.findMany({
     where: {
@@ -44,5 +64,46 @@ export const createProductAction = async (
   prevState: any,
   formData: FormData
 ): Promise<{ message: string }> => {
-  return await { message: "product created!" };
+  const user = await getAuthUser();
+  try {
+    const name = formData.get("name") as string;
+    const company = formData.get("company") as string;
+    const price = Number(formData.get("price") as string);
+    let image = formData.get("image") as File;
+    const description = formData.get("description") as string;
+    const featured = Boolean(formData.get("featured") as string);
+    const arrayBuffer = await image.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
+    const uploadResult = await new Promise((resolve, reject) => {
+      const uploadStream = cloudinary.uploader.upload_stream(
+        {
+          folder: "storeFront",
+          resource_type: "image",
+        },
+        (error, result) => {
+          if (error) reject(error);
+          else resolve(result);
+        }
+      );
+      uploadStream.end(buffer);
+    });
+
+    const imageUrl = uploadResult.secure_url;
+    image = imageUrl;
+    console.log(image);
+    await prisma.product.create({
+      data: {
+        name,
+        company,
+        price,
+        image,
+        description,
+        featured,
+        clerkId: user.id,
+      },
+    });
+    return { message: "product created" };
+  } catch (error) {
+    return renderError(error);
+  }
 };
