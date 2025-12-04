cf72e9ef11b3d33854f0d10dbb08f97cc433bd17
alijamshidy <kbxxkj@gmail.com>
2025-12-04 21:59:26 +0330

    Add PriceInput component for reusable price input field with label support

---
+++ components/form/PriceInput.tsx
@@ -0,0 +1,26 @@
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+
+const name = "price";
+type FormInputNumberProps = {
+  defaultValue?: number;
+};
+function PriceInput({ defaultValue }: FormInputNumberProps) {
+  return (
+    <div className="mb-2">
+      <Label
+        className="capitalize mb-2"
+        htmlFor={name}>
+        Price ($)
+      </Label>
+      <Input
+        type="number"
+        id={name}
+        name={name}
+        defaultValue={defaultValue || 100}
+        min={0}
+        required
+      />
+    </div>
+  );
+}
