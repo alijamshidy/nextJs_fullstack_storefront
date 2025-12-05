ce22d53d65d33725fa68e354b563b155c0cf4cc5
alijamshidy <kbxxkj@gmail.com>
2025-12-05 22:45:39 +0330

    Add productSchema and validation utility using Zod for enhanced data validation in product creation

---
+++ utils/schemas.ts
@@ -0,0 +1,35 @@
+import { z, ZodSchema } from "zod";
+
+export const productSchema = z.object({
+  name: z
+    .string()
+    .min(2, { message: "name must be at least 2 characters." })
+    .max(100, { message: "name must be less than 100 characters." }),
+  company: z.string(),
+  price: z.coerce
+    .number()
+    .int()
+    .min(0, { message: "price must be a positive number" }),
+
+  description: z.string().refine(
+    description => {
+      const wordCount = description.split(" ").length;
+      return wordCount >= 10 && wordCount <= 1000;
+    },
+    {
+      message: "description must be between 10 and 1000 words",
+    }
+  ),
+});
+export function validateWithZodSchema<T>(
+  schema: ZodSchema<T>,
+  data: unknown
+): T {
+  const result = schema.safeParse(data);
+  if (!result.success) {
+    const errors = result.error.issues.map(issue => issue.message);
+    throw new Error(errors.join(", "));
+  }
+  console.log(result);
+  return result.data;
+}
