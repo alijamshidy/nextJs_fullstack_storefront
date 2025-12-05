c66a9ecf21c631483735ebc973f056ae8c67a2f9
alijamshidy <kbxxkj@gmail.com>
2025-12-05 23:40:14 +0330

    Implement AdminProductsPage component to display a table of products with fetching logic and empty state handling.

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,3 +1,55 @@
-export default function ProductPage() {
-  return <div>Admin ProductPage</div>;
+import EmptyList from "@/components/global/EmptyList";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { fetchAdminProducts } from "@/utils/actions";
+import { formatCurrency } from "@/utils/format";
+import Link from "next/link";
+
+export default async function AdminProductsPage() {
+  const items = await fetchAdminProducts();
+  if (items.length === 0) return <EmptyList />;
+
+  return (
+    <section>
+      <Table>
+        <TableCaption className="capitalize">
+          total products : {items.length}
+        </TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Product Name</TableHead>
+            <TableHead>Company</TableHead>
+            <TableHead>Price</TableHead>
+            <TableHead>Actions</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {items.map(item => {
+            const { id: productId, name, company, price } = item;
+            return (
+              <TableRow key={productId}>
+                <TableCell>
+                  <Link
+                    href={`/products/${productId}`}
+                    className="underline text-muted-foreground traking-wide capitalize">
+                    {name}
+                  </Link>
+                </TableCell>
+                <TableCell>{company}</TableCell>
+                <TableCell>{formatCurrency(price)}</TableCell>
+                <TableCell className="flex items-center gap-x-2"></TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </section>
+  );
 }
