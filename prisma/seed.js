5abdf12bd041856f1a0a3d93579915ef8e9d575e
alijamshidy <kbxxkj@gmail.com>
2025-12-09 04:01:01 +0330

    Refactor seed script to use PostgreSQL connection pool and Prisma adapter for improved database management

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,7 +1,22 @@
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
-import products from "./products.json" assert { type: "json" };
+import pkg from "pg";
+const { Pool } = pkg;
 
-const prisma = new PrismaClient();
+import products from "./products.json" with { type: "json" };
+
+// ساخت pool
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+// ساخت Adapter
+const adapter = new PrismaPg(pool);
+
+// PrismaClient با adapter
+const prisma = new PrismaClient({
+  adapter,
+});
 
 async function main() {
   for (const product of products) {
@@ -10,12 +25,15 @@ async function main() {
     });
   }
 }
+
 main()
   .then(async () => {
     await prisma.$disconnect();
+    await pool.end();
   })
-  .catch(async e => {
+  .catch(async (e) => {
     console.error(e);
     await prisma.$disconnect();
+    await pool.end();
     process.exit(1);
   });
