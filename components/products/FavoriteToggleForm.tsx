7badf44002d255754fd207733cbd168de985a825
alijamshidy <kbxxkj@gmail.com>
2025-12-09 04:58:59 +0330

    Implement FavoriteToggleForm component to manage favorite state with toggle functionality, utilizing FormContainer and CardSubmitButton for improved user interaction.

--- components/products/FavoriteToggleForm.tsx
+++ components/products/FavoriteToggleForm.tsx
@@ -1,3 +1,28 @@
-export default function FavoriteToggleForm() {
-  return <div>FavoriteToggleForm</div>;
+"use client";
+
+import { toggleFavoriteAction } from "@/utils/actions";
+import { usePathname } from "next/navigation";
+import { CardSubmitButton } from "../form/Buttons";
+import FormContainer from "../form/FormContainer";
+
+type FavoriteToggleFormProps = {
+  productId: string;
+  favoriteId: string | null;
+};
+
+export default function FavoriteToggleForm({
+  productId,
+  favoriteId,
+}: FavoriteToggleFormProps) {
+  const pathName = usePathname();
+  const toggleAction = toggleFavoriteAction.bind(null, {
+    productId,
+    favoriteId,
+    pathName,
+  });
+  return (
+    <FormContainer action={toggleAction}>
+      <CardSubmitButton isFavorite={favoriteId ? true : false} />
+    </FormContainer>
+  );
 }
