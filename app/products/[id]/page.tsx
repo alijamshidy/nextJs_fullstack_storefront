6e53ed45bbaf0b8642f9d8126f15c17ad7e280f1
alijamshidy <kbxxkj@gmail.com>
2025-12-10 01:54:58 +0330

    Add ShareButton to SingleProductPage for enhanced product sharing options, allowing users to share via social media and email.

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -2,6 +2,7 @@ import FavoriteToggleButton from "@/components/products/FavoriteToggleButton";
 import AddToCart from "@/components/single-product/AddToCart";
 import BreadCrumbs from "@/components/single-product/BreadCrumbs";
 import ProductRating from "@/components/single-product/ProductRating";
+import ShareButton from "@/components/single-product/ShareButton";
 import { fetchSingleProduct } from "@/utils/actions";
 import { formatCurrency } from "@/utils/format";
 import Image from "next/image";
@@ -34,7 +35,13 @@ export default async function SingleProductPage({
         <div>
           <div className="flex gap-x-8 items-center">
             <h1 className="capitaalize text-3xl font-bold">{name}</h1>
-            <FavoriteToggleButton productId={id} />
+            <div className="flex items-center gap-x-2">
+              <FavoriteToggleButton productId={id} />
+              <ShareButton
+                name={product.name}
+                productId={id}
+              />
+            </div>
           </div>
           <ProductRating productId={id} />
           <h4 className="text-xl mt-2">{company}</h4>
