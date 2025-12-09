68818d1927e9952a343235b1a01dbf5f3115f7d3
alijamshidy <kbxxkj@gmail.com>
2025-12-10 01:01:35 +0330

    Implement FavoritesPage to display user favorites, including a message for empty favorites and a grid for displaying favorite products.

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,3 +1,15 @@
-export default function FavoritesPage() {
-  return <div>FavoritesPage</div>;
+import SectionTitle from "@/components/global/SectionTitle";
+import ProductsGrid from "@/components/products/ProductsGrid";
+import { fetchUserFavorites } from "@/utils/actions";
+
+export default async function FavoritesPage() {
+  const favorites = await fetchUserFavorites();
+  if (favorites.length === 0)
+    return <SectionTitle text="You have no favorites yet." />;
+  return (
+    <div>
+      <SectionTitle text="Favorites" />
+      <ProductsGrid products={favorites.map(favorite => favorite.product)} />
+    </div>
+  );
 }
