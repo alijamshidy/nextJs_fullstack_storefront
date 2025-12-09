5daf4df3c905bbd5d01c72b1b2cdbad170198c83
alijamshidy <kbxxkj@gmail.com>
2025-12-09 04:58:45 +0330

    Refactor FavoriteToggleButton to integrate authentication and fetch favorite ID, replacing button with FavoriteToggleForm for enhanced user experience.

--- components/products/FavoriteToggleButton.tsx
+++ components/products/FavoriteToggleButton.tsx
@@ -1,16 +1,20 @@
-import { FaHeart } from "react-icons/fa";
-import { Button } from "../ui/button";
-export default function FavoriteToggleButton({
+import { fetchFavoriteId } from "@/utils/actions";
+import { auth } from "@clerk/nextjs/server";
+import { CardSignInButton } from "../form/Buttons";
+import FavoriteToggleForm from "./FavoriteToggleForm";
+export default async function FavoriteToggleButton({
   productId,
 }: {
   productId: string;
 }) {
+  const { userId } = await auth();
+  if (!userId) <CardSignInButton />;
+  const favoriteId = await fetchFavoriteId({ productId });
+
   return (
-    <Button
-      size={"icon"}
-      variant={"outline"}
-      className="p-2 cursor-pointer">
-      <FaHeart />
-    </Button>
+    <FavoriteToggleForm
+      favoriteId={favoriteId}
+      productId={productId}
+    />
   );
 }
