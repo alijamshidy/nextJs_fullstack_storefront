8aca633a5c580404c22a880e5ff376d8ccf5c0ea
alijamshidy <kbxxkj@gmail.com>
2025-12-10 05:28:01 +0330

    Add fetchProductReviewsByUser and deleteReviewAction functions to actions.ts for user-specific review retrieval and deletion. Enhance user interaction by allowing users to manage their reviews effectively.

--- utils/actions.ts
+++ utils/actions.ts
@@ -285,8 +285,42 @@ export const fetchProductReviews = async (productId: string) => {
   });
   return reviews;
 };
-export const fetchProductReviewsByUser = async () => {};
-export const deleteReviewAction = async () => {};
+export const fetchProductReviewsByUser = async () => {
+  const user = await getAuthUser();
+  const reviews = await prisma.review.findMany({
+    where: {
+      clerkId: user.id,
+    },
+    select: {
+      id: true,
+      rating: true,
+      comment: true,
+      product: {
+        select: {
+          image: true,
+          name: true,
+        },
+      },
+    },
+  });
+  return reviews;
+};
+export const deleteReviewAction = async (prevState: { reviewId: string }) => {
+  const { reviewId } = prevState;
+  const user = await getAuthUser();
+  try {
+    await prisma.review.delete({
+      where: {
+        id: reviewId,
+        clerkId: user.id,
+      },
+    });
+    revalidatePath("/reviews");
+    return { message: "review deleted successfully" };
+  } catch (error) {
+    return renderError(error);
+  }
+};
 export const findExistingReview = async () => {};
 export const fetchProductRating = async (productId: string) => {
   const result = await prisma.review.groupBy({
