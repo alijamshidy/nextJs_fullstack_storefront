de2c0a4949d7743b731ba062114b4587f03af661
alijamshidy <kbxxkj@gmail.com>
2025-12-10 04:57:04 +0330

    Refactor ProductRating component to fetch rating and review count asynchronously using fetchProductRating function, improving data accuracy and responsiveness.

--- components/single-product/ProductRating.tsx
+++ components/single-product/ProductRating.tsx
@@ -1,8 +1,12 @@
+import { fetchProductRating } from "@/utils/actions";
 import { FaStar } from "react-icons/fa";
 
-export default function ProductRating({ productId }: { productId: string }) {
-  const rating = 4.2;
-  const count = 25;
+export default async function ProductRating({
+  productId,
+}: {
+  productId: string;
+}) {
+  const { count, rating } = await fetchProductRating(productId);
   const calssName = "flex gap-1 items-center text-md mt-1 mb-4";
   const countValue = `(${count} reviews)`;
   return (
