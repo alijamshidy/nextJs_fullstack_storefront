d1a4421b46a5bf554d82483003213c9e81b9ef64
alijamshidy <kbxxkj@gmail.com>
2025-12-12 17:46:12 +0330

    Add ThirdColumn component to manage product quantity and removal in the cart. This component integrates SelectProductAmount for quantity selection and a FormContainer for item removal, enhancing cart item management and user interaction.

---
+++ components/cart/ThirdColumn.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { removeCartItemAction } from "@/utils/actions";
+import { useState } from "react";
+import SubmitButton from "../form/Buttons";
+import FormContainer from "../form/FormContainer";
+import SelectProductAmount, {
+  Mode,
+} from "../single-product/SelectProductAmount";
+
+export default function ThirdColumn({
+  quantity,
+  id,
+}: {
+  quantity: number;
+  id: string;
+}) {
+  const [amount, setAmount] = useState(quantity);
+  const handleAmountChange = async (value: number) => {
+    setAmount(value);
+  };
+
+  return (
+    <div className="md:ml-8">
+      <SelectProductAmount
+        amount={amount}
+        setAmount={handleAmountChange}
+        mode={Mode.CartItem}
+        isLoading={false}
+      />
+      <FormContainer action={removeCartItemAction}>
+        <input
+          type="hidden"
+          name="id"
+          value={id}
+        />
+        <SubmitButton
+          size={"sm"}
+          className="mt-4"
+          text="remove"
+        />
+      </FormContainer>
+    </div>
+  );
+}
