29046770e9f0eff2693fdc1b6eb1c784c53bb0fe
alijamshidy <kbxxkj@gmail.com>
2025-12-12 16:39:42 +0330

    Add CartTotals component to display cart summary and order total. Integrate CartTotalRow for individual item display and include a Place Order button to facilitate order creation, enhancing the checkout process.

---
+++ components/cart/CartTotals.tsx
@@ -0,0 +1,62 @@
+import { createOrderAction } from "@/utils/actions";
+import { formatCurrency } from "@/utils/format";
+import { Cart } from "@prisma/client";
+import SubmitButton from "../form/Buttons";
+import FormContainer from "../form/FormContainer";
+import { Card, CardTitle } from "../ui/card";
+import { Separator } from "../ui/separator";
+
+export default function CartTotals({ cart }: { cart: Cart }) {
+  const { cartTotal, shipping, tax, orderTotal } = cart;
+  return (
+    <div>
+      <Card className="p-8 gap-1">
+        <CartTotalRow
+          label="Subtotal"
+          amount={cartTotal}
+        />
+        <CartTotalRow
+          label="Shipping"
+          amount={shipping}
+        />
+        <CartTotalRow
+          label="tax"
+          amount={tax}
+        />
+        <CardTitle className="mt-8">
+          <CartTotalRow
+            label="Order Total"
+            amount={orderTotal}
+            lastRow
+          />
+        </CardTitle>
+      </Card>
+      <FormContainer action={createOrderAction}>
+        <SubmitButton
+          text="Place Order"
+          className="w-full mt-8"
+        />
+      </FormContainer>
+    </div>
+  );
+}
+
+function CartTotalRow({
+  label,
+  amount,
+  lastRow,
+}: {
+  label: string;
+  amount: number;
+  lastRow?: boolean;
+}) {
+  return (
+    <>
+      <p className="flex justify-between text-sm">
+        <span>{label}</span>
+        <span>{formatCurrency(amount)}</span>
+      </p>
+      {lastRow ? null : <Separator className="my-2" />}
+    </>
+  );
+}
