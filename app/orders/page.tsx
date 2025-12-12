9bdf67e36e2832bc560ed6297fded57c91603356
alijamshidy <kbxxkj@gmail.com>
2025-12-12 20:27:16 +0330

    Add loading component and enhance OrdersPage to display user orders in a structured table format. The new loading component utilizes LoadingTable for better user experience, while OrdersPage fetches and formats order data, improving order visibility and management.

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -1,3 +1,49 @@
-export default function OrdersPage() {
-  return <div>OrdersPage</div>;
+import SectionTitle from "@/components/global/SectionTitle";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { fetchUserOrders } from "@/utils/actions";
+import { formatCurrency, formatDate } from "@/utils/format";
+
+export default async function OrdersPage() {
+  const orders = await fetchUserOrders();
+
+  return (
+    <>
+      <SectionTitle text="Your Orders" />
+      <Table>
+        <TableCaption>Total Orders : {orders.length}</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Products</TableHead>
+            <TableHead>Order Total</TableHead>
+            <TableHead>Tax</TableHead>
+            <TableHead>Shipping</TableHead>
+            <TableHead>Date</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {orders.map(order => {
+            const { id, products, orderTotal, tax, shipping, createdAt } =
+              order;
+            return (
+              <TableRow key={order.id}>
+                <TableCell>{products}</TableCell>
+                <TableCell>{formatCurrency(orderTotal)}</TableCell>
+                <TableCell>{formatCurrency(tax)}</TableCell>
+                <TableCell>{formatCurrency(shipping)}</TableCell>
+                <TableCell>{formatDate(createdAt)}</TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </>
+  );
 }
