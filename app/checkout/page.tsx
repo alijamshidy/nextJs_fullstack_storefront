2aa3b7ee6246d67fadf0a0f61e689e0f03da8a3c
alijamshidy <kbxxkj@gmail.com>
2025-12-14 18:05:31 +0330

    Add CheckoutPage component to handle Stripe embedded checkout. Integrate client secret fetching based on order and cart IDs from URL parameters, enhancing payment processing capabilities.

---
+++ app/checkout/page.tsx
@@ -0,0 +1,39 @@
+"use client";
+import {
+  EmbeddedCheckout,
+  EmbeddedCheckoutProvider,
+} from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import axios from "axios";
+import { useSearchParams } from "next/navigation";
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string
+);
+
+import { useCallback } from "react";
+
+export default function CheckoutPage() {
+  const searchParams = useSearchParams();
+
+  const orderId = searchParams.get("orderId");
+  const cartId = searchParams.get("cartId");
+
+  const fetchClientSecret = useCallback(async () => {
+    const response = await axios.post("/api/payment", {
+      orderId,
+      cartId,
+    });
+    return response.data.clientSecret;
+  }, [orderId, cartId]);
+
+  const options = { fetchClientSecret };
+  return (
+    <div className="checkout">
+      <EmbeddedCheckoutProvider
+        stripe={stripePromise}
+        options={options}>
+        <EmbeddedCheckout />
+      </EmbeddedCheckoutProvider>
+    </div>
+  );
+}
